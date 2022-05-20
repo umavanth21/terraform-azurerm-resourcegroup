4fe12b81b8012499495ef8c2eac8c2abb0c941b9
umavanth21 <56533761+umavanth21@users.noreply.github.com>
2022-05-20 17:57:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,24 @@
-resource "azurerm_resource_group" "res_group" {
-  location = "${var.location}"
-  name     = "${var.name}"
-
-  tags {
-    environment = "Test"
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=2.75.0"
+    }
   }
+}
+
+provider "azurerm" {
+  features {}
+
+  # More information on the authentication methods supported by
+  # the AzureRM Provider can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+data "azurerm_resource_group" "hcmxexample" {
+  name     = var.resource_group_name
 }
