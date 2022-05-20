448462466fbf267b6b9fa3ae6c18e7c0cddf55ab
umavanth21 <56533761+umavanth21@users.noreply.github.com>
2022-05-20 18:18:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,4 +6,18 @@ variable "location" {
 variable "resource_group_name" {
   description = "Resource Group Name"
 }
+variable "subscription_id" {
+ default = "subscription_id"
+  }
 
+variable "client_id" {
+ default = "client_id"
+  }
+
+variable "tenant_id" {
+  default = "tenant_id"
+  }
+
+variable "client_secret" {
+  default = "client_secret"
+  }
